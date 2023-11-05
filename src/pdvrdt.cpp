857312bfa26bb00d4477f5f0306ccc74103b70a4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2023-11-05 10:28:16 +0000

    Update pdvrdt.cpp

--- src/pdvrdt.cpp
+++ src/pdvrdt.cpp
@@ -20,7 +20,7 @@ struct PDV_STRUCT {
 	std::string image_file, data_file;
 	uint64_t image_size{}, data_size{};
 	uint8_t file_count{}, sub_file_count{};
-	bool insert_file = false, extract_file = false, inflate_data = false, deflate_data = false;
+	bool insert_file = false, extract_file = false, reddit_opt = false, inflate_data = false, deflate_data = false;
 };
 
 // Update values, such as chunk lengths, file sizes & other values. Write them into the relevant vector index locations. Overwrites previous values.
@@ -63,18 +63,33 @@ int main(int argc, char** argv) {
 		argc = 0;
 		Display_Info();
 	}
-	else if (argc >= 4 && argc < 10 && std::string(argv[1]) == "-i") { // Insert file mode.
-		pdv.insert_file = true, pdv.deflate_data = true, pdv.sub_file_count = argc - 1, pdv.image_file = argv[2];
-		argc -= 2;
+	else if (argc >= 4 && argc < 10 && std::string(argv[1]) == "-i") {
+		
+		// Insert file mode.
+
+		if (std::string(argv[2]) == "-r") {
+			pdv.reddit_opt = true;
+		}
+		pdv.insert_file = true;
+		pdv.deflate_data = true,
+		pdv.sub_file_count = pdv.reddit_opt ? argc - 2 : argc - 1;
+		pdv.image_file = pdv.reddit_opt ? argv[3] : argv[2];
+		pdv.reddit_opt ? argc -= 3 : argc -=2;
+
 		while (argc != 1) {
-			pdv.file_count = argc, pdv.data_file = argv[3];
+			pdv.file_count = argc;
+			pdv.data_file = pdv.reddit_opt ? argv[4] : argv[3];
 			Open_Files(argv++, pdv);
 			argc--;
 		}
 		argc = 1;
 	}
-	else if (argc >= 3 && argc < 9 && std::string(argv[1]) == "-x") { // Extract file mode.
+	else if (argc >= 3 && argc < 9 && std::string(argv[1]) == "-x") { 
+		
+		// Extract file mode.
+
 		pdv.extract_file = true;
+
 		while (argc >= 3) {
 			pdv.image_file = argv[2];
 			Open_Files(argv++, pdv);
@@ -82,7 +97,7 @@ int main(int argc, char** argv) {
 		}
 	}
 	else {
-		std::cerr << "\nUsage:\tpdvrdt -i <png-image> <file(s)>\n\tpdvrdt -x <png-image(s)>\n\tpdvrdt --info\n\n";
+		std::cerr << "\nUsage:\tpdvrdt -i [-r] <png-image> <file(s)>\n\tpdvrdt -x <png-image(s)>\n\tpdvrdt --info\n\n";
 		argc = 0;
 	}
 	if (argc != 0) {
@@ -113,7 +128,9 @@ void Open_Files(char* argv[], PDV_STRUCT& pdv) {
 		std::exit(EXIT_FAILURE);
 	}
 
-	const std::string START_MSG = pdv.insert_file ? "\nInsert mode selected.\n\nReading files. Please wait...\n" : "\nExtract mode selected.\n\nReading embedded PNG image file. Please wait...\n";
+	const std::string START_MSG = pdv.insert_file && pdv.reddit_opt ? "\nInsert mode selected with -r option.\n\nReading files. Please wait...\n" 
+						: pdv.insert_file ? "\nInsert mode selected.\n\nReading files. Please wait...\n" 
+						: "\nExtract mode selected.\n\nReading embedded PNG image file. Please wait...\n";
 
 	std::cout << START_MSG;
 
@@ -154,7 +171,8 @@ void Open_Files(char* argv[], PDV_STRUCT& pdv) {
 			std::exit(EXIT_FAILURE);
 		}
 
-		// Find and remove unwanted chunks. Some media sites add various PNG chunks to the downloaded image, which could cause issues with the expected location of the iCCP chunk.
+		// Find and remove unwanted chunks. 
+		// Some media sites add various PNG chunks to the downloaded image, which could cause issues with the expected location of the iCCP chunk.
 		Erase_Chunks(pdv);
 
 		if (GET_PROFILE_SIG != PROFILE_SIG)  {  // An iCCP chunk was found somewhere within the image, but wasn't in the correct location, so not a pdvrdt data-embedded image.
@@ -169,6 +187,8 @@ void Open_Files(char* argv[], PDV_STRUCT& pdv) {
 		const uint8_t
 			PROFILE_CHUNK_SIZE_INDEX = 33,	// Start index location of 4 byte iCCP Profile chunk length field.
 			DEFLATE_DATA_INDEX = 46;	// Start index location of deflate data within ICCP Profile chunk. (78, 9C...).
+
+		pdv.Image_Vec[DEFLATE_DATA_INDEX] = '\x78'; // Put back 0x78 char, just in case is was removed by the reddit option (-r). 
 
 		const uint32_t
 			// Get iCCP Profile chunk length.
@@ -318,7 +338,7 @@ void Encrypt_Decrypt(PDV_STRUCT& pdv) {
 		else {
 			xor_key_pos = xor_key_pos > XOR_KEY_LENGTH ? XOR_KEY_START_POS : xor_key_pos;	// Reset XOR_KEY position to the start if it has reached last character.
 			output_name += INPUT_NAME[index_pos] ^ XOR_KEY[xor_key_pos++];			// XOR each character of filename against characters of XOR_KEY string. Store output characters in "output_name".
-																							// Depending on Mode, filename is either encrypted or decrypted.
+													// Depending on Mode, filename is either encrypted or decrypted.
 		}
 
 		if (pdv.insert_file) {
@@ -359,6 +379,10 @@ void Encrypt_Decrypt(PDV_STRUCT& pdv) {
 
 		// Call function to deflate/compress the contents of vector "Profile_Data_Vec" (profile with user's encrypted data file).
 		Inflate_Deflate(pdv.Profile_Data_Vec, pdv.deflate_data);
+
+		if (pdv.reddit_opt) {
+			pdv.Profile_Data_Vec[0] = '\x00';
+		}
 
 		const uint32_t
 			PROFILE_DEFLATE_SIZE = static_cast<uint32_t>(pdv.Profile_Data_Vec.size()),
@@ -537,28 +561,28 @@ uint32_t Crc_Update(const uint32_t Crc, BYTE* buf, const uint32_t len)
 {
 		// Table of CRCs of all 8-bit messages.
 		std::vector<uint32_t> Crc_Table_Vec = {
-			0x00, 	    0x77073096, 0xEE0E612C, 0x990951BA, 0x76DC419,  0x706AF48F, 0xE963A535, 0x9E6495A3, 0xEDB8832,  0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x9B64C2B,  0x7EB17CBD,
-			0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7, 0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC,
-			0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5, 0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B, 0x35B5A8FA, 0x42B2986C,
-			0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59, 0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F,
-			0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D, 0x76DC4190, 0x1DB7106,  0x98D220BC, 0xEFD5102A, 0x71B18589, 0x6B6B51F,
-			0x9FBFE4A5, 0xE8B8D433, 0x7807C9A2, 0xF00F934, 0x9609A88E,  0xE10E9818, 0x7F6A0DBB, 0x86D3D2D,  0x91646C97, 0xE6635C01, 0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E,
-			0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457, 0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65, 0x4DB26158, 0x3AB551CE,
-			0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB, 0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9,
-			0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F, 0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81,
-			0xB7BD5C3B, 0xC0BA6CAD, 0xEDB88320, 0x9ABFB3B6, 0x3B6E20C,  0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x4DB2615,  0x73DC1683, 0xE3630B12, 0x94643B84, 0xD6D6A3E,  0x7A6A5AA8,
-			0xE40ECF0B, 0x9309FF9D, 0xA00AE27,  0x7D079EB1, 0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7, 0xFED41B76, 0x89D32BE0,
-			0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5, 0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
-			0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79, 0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703,
-			0x220216B9, 0x5505262F, 0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D, 0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x26D930A,
-			0x9C0906A9, 0xEB0E363F, 0x72076785, 0x5005713,  0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0xCB61B38,  0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0xBDBDF21,  0x86D3D2D4, 0xF1D4E242,
-			0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777, 0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45,
-			0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB, 0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5,
-			0x47B2CF7F, 0x30B5FFE9, 0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94,
-			0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D };
+		0x00, 	    0x77073096, 0xEE0E612C, 0x990951BA, 0x76DC419,  0x706AF48F, 0xE963A535, 0x9E6495A3, 0xEDB8832,  0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x9B64C2B,  0x7EB17CBD,
+		0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7, 0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC,
+		0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5, 0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B, 0x35B5A8FA, 0x42B2986C,
+		0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59, 0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F,
+		0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D, 0x76DC4190, 0x1DB7106,  0x98D220BC, 0xEFD5102A, 0x71B18589, 0x6B6B51F,
+		0x9FBFE4A5, 0xE8B8D433, 0x7807C9A2, 0xF00F934,  0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x86D3D2D, 0x91646C97,  0xE6635C01, 0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E,
+		0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457, 0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65, 0x4DB26158, 0x3AB551CE,
+		0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB, 0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9,
+		0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F, 0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81,
+		0xB7BD5C3B, 0xC0BA6CAD, 0xEDB88320, 0x9ABFB3B6, 0x3B6E20C,  0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x4DB2615,  0x73DC1683, 0xE3630B12, 0x94643B84, 0xD6D6A3E,  0x7A6A5AA8,
+		0xE40ECF0B, 0x9309FF9D, 0xA00AE27,  0x7D079EB1, 0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7, 0xFED41B76, 0x89D32BE0,
+		0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5, 0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
+		0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79, 0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703,
+		0x220216B9, 0x5505262F, 0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D, 0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x26D930A,
+		0x9C0906A9, 0xEB0E363F, 0x72076785, 0x5005713,  0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0xCB61B38,  0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0xBDBDF21,  0x86D3D2D4, 0xF1D4E242,
+		0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777, 0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45,
+		0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB, 0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5,
+		0x47B2CF7F, 0x30B5FFE9, 0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94,
+		0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D };
 
 	// Update a running crc with the bytes buf[0..len - 1] the crc should be initialized to all 1's, 
-	// and the transmitted value is the 1's complement of the final running crc (see the Crc() routine below).
+	// and the transmitted value is the 1's complement of the final running crc (see the crc() routine below).
 	uint32_t c = Crc;
 
 	for (uint32_t n{}; n < len; n++) {
@@ -601,22 +625,27 @@ void Write_Out_File(PDV_STRUCT& pdv) {
 			// Twitter 9.5KB. While not officially supported because of the tiny size requirement, if your data file is this size 
 			// (9.5KB, 9836 bytes) or lower with image dimensions 900x900 or less (PNG-32/24) 4096x4096 or less (PNG-8), 
 			// then you should be able to use Twitter to share your data-embedded image.
-			REDDIT_SIZE =	 1048172,	// Just under 1MB.
-			IMGUR_SIZE =	 5242880,	// 5MB
-			IMG_PILE_SIZE =  8388608,	// 8MB
-			MASTODON_SIZE =  16777216,	// 16MB
-			POST_IMG_SIZE =	 25165824,	// 24MB
-			IMGBB_SIZE =	 33554432;	// 32MB
+			REDDIT_SIZE =		 1048172,	// Just under 1MB. (Default size without the -r option).
+			IMGUR_SIZE =		 5242880,	// 5MB
+			IMG_PILE_SIZE =		 8388608,	// 8MB
+			MASTODON_SIZE =		 16777216,	// 16MB
+			REDDIT_OPTION_SIZE = 	 20971520,  	// 20MB (Only with the -r option selected).
+			POST_IMG_SIZE =		 25165824,	// 24MB
+			IMGBB_SIZE =		 33554432;	// 32MB
 			// Flickr is 200MB, this programs max size, no need to make a variable for it.
 		
 		size_warning = (IMG_SIZE > IMGUR_SIZE && IMG_SIZE <= IMG_PILE_SIZE ? size_warning.substr(0, MSG_LEN - 8)
-				: (IMG_SIZE > IMG_PILE_SIZE && IMG_SIZE <= MASTODON_SIZE ? size_warning.substr(0, MSG_LEN - 17)
-				: (IMG_SIZE > MASTODON_SIZE && IMG_SIZE <= POST_IMG_SIZE ? size_warning.substr(0, MSG_LEN - 27)
-				: (IMG_SIZE > POST_IMG_SIZE && IMG_SIZE <= IMGBB_SIZE ? size_warning.substr(0, MSG_LEN - 38)
-				: (IMG_SIZE > IMGBB_SIZE ? size_warning.substr(0, MSG_LEN - 45) : size_warning)))));
+						: (IMG_SIZE > IMG_PILE_SIZE && IMG_SIZE <= MASTODON_SIZE ? size_warning.substr(0, MSG_LEN - 17)
+						: (IMG_SIZE > MASTODON_SIZE && IMG_SIZE <= POST_IMG_SIZE ? size_warning.substr(0, MSG_LEN - 27)
+						: (IMG_SIZE > POST_IMG_SIZE && IMG_SIZE <= IMGBB_SIZE ? size_warning.substr(0, MSG_LEN - 38)
+						: (IMG_SIZE > IMGBB_SIZE ? size_warning.substr(0, MSG_LEN - 45) : size_warning)))));
 
-		if (pdv.data_size > REDDIT_SIZE) {
+		if (pdv.data_size > REDDIT_SIZE && !pdv.reddit_opt) {
 			std::cerr << size_warning << ".\n";
+		}
+
+		if (pdv.data_size <= REDDIT_OPTION_SIZE && pdv.reddit_opt) {
+			std::cout << "\n**Warning**\n\nDue to the selection of the -r option, you can only post this data-embedded PNG image on Reddit.\n";
 		}
 
 		std::cout << "\nCreated data-embedded PNG image: \"" + pdv.data_file + "\" Size: \"" << pdv.Image_Vec.size() << " Bytes\"\n";
@@ -647,26 +676,31 @@ Share your data-embedded image on the following compatible sites.
 
 Image size limit is platform dependant:-  
 
-  Flickr (200MB), ImgBB (32MB), PostImage (24MB), Mastodon (16MB), 
-  *ImgPile (8MB), Imgur (5MB), *Reddit (1MB Desktop/Browser only), *Twitter (10KB).
+  Flickr (200MB), ImgBB (32MB), PostImage (24MB), *Reddit (20MB / with -r option), Mastodon (16MB), 
+  *ImgPile (8MB), Imgur (5MB), *Reddit (1MB / without -r option), *Twitter (10KB).
 
 Options: [-i] File insert mode.
 	 [-x] File extract mode.
+ 	 [-r] Reddit option, used with -i insert mode (e.g. $ pdvrdt -i -r image_file.png music_file.mp3).
+
+Using the -r option when embedding a data file increases the Reddit upload size limit from 1MB to 20MB.
+The data-embedded PNG image created with the -r option can only be shared on Reddit and is incompatible
+with the other platforms listed above.
 
 *ImgPile - You must sign in to an account before sharing your data-embedded PNG image on this platform.
 Sharing your image without logging in, your embedded data will not be preserved.
 
-To embed larger files for Twitter (up to 5MB), please use pdvzip.
-To embed larger files for Reddit (up to 20MB), please use jdvrif. You can also use jdvrif for Mastodon (up to 16MB).
+To embed and share larger files for Twitter (up to 5MB), please use pdvzip.
 
 This program works on Linux and Windows.
 
-For Flickr, Mastodon, ImgBB, PostImage, Mastodon, ImgPile & Imgur,
+For Flickr, ImgBB, PostImage, *Reddit (with -r option), Mastodon, ImgPile & Imgur,
 the size limit is measured by the total size of your data-embedded PNG image file. 
 
-For Reddit & Twitter, the size limit is measured by the uncompressed size of the ICC Profile, where your data is stored.
+For *Reddit (without -r option) & Twitter, the size limit is measured by the 
+uncompressed size of the ICC Profile, where your data is stored.
 
-Reddit: 1,048,172 bytes is the uncompressed (zlib inflate) size limit for your data file.
+Reddit (without -r option): 1,048,172 bytes is the uncompressed (zlib inflate) size limit for your data file.
 404 bytes are used for the basic ICC Profile. (404 + 1048172 = 1,048,576 bytes [1MB]).
 
 Twitter: 9,836 bytes is the uncompressed (zlib inflate) limit for your data file.
@@ -680,3 +714,4 @@ You can also extract files from up to six images at a time.
 
 )";
 }
+
