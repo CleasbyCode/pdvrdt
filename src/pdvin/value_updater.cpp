d1d049bc5259d19ca742561890e581fb513d51a4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-15 23:01:26 +0100

    Update value_updater.cpp

--- src/pdvin/value_updater.cpp
+++ src/pdvin/value_updater.cpp
@@ -2,6 +2,6 @@
 
 void valueUpdater(std::vector<uchar>& vec, uint32_t value_insert_index, const uint32_t NEW_VALUE, uint8_t value_bit_length) {
 	while (value_bit_length) {
-		static_cast<size_t>(vec[value_insert_index++] = (NEW_VALUE >> (value_bit_length -= 8)) & 0xff);
+		static_cast<uint32_t>(vec[value_insert_index++] = (NEW_VALUE >> (value_bit_length -= 8)) & 0xff);
 	}
 }
