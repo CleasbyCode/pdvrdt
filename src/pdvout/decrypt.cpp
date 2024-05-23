c065ac4824e88a036ab4df01057b93884b024b48
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-23 02:42:04 +0100

    Add files via upload

---
+++ src/pdvout/decrypt.cpp
@@ -0,0 +1,35 @@
+
+void decryptFile(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fast8_t>&File_Vec, std::string& encrypted_file_name) {
+
+	std::string decrypted_file_name;
+
+	constexpr uint_fast8_t XOR_KEY_LENGTH = 6;
+
+	const uint_fast8_t FILE_NAME_LENGTH = static_cast<uint_fast8_t>(encrypted_file_name.length());
+
+	uint_fast32_t
+		file_size = static_cast<uint_fast32_t>(Image_Vec.size()),
+		index_pos{};
+
+	uint_fast8_t
+		xor_key[XOR_KEY_LENGTH] { 0xFC, 0xD8, 0xF9, 0xE2, 0x9F, 0x7E },
+		xor_key_pos{},
+		name_key_pos{};
+	
+	while (file_size > index_pos) {
+		
+		std::reverse(std::begin(xor_key), std::end(xor_key));
+		
+		if (index_pos >= FILE_NAME_LENGTH) {
+			name_key_pos = name_key_pos > FILE_NAME_LENGTH ? 0 : name_key_pos;
+		}
+		else {
+			xor_key_pos = xor_key_pos > XOR_KEY_LENGTH ? 0 : xor_key_pos;
+			decrypted_file_name += encrypted_file_name[index_pos] ^ xor_key[xor_key_pos++];
+		}
+
+		File_Vec.emplace_back(Image_Vec[index_pos++] ^ encrypted_file_name[name_key_pos++]);
+	}
+
+	encrypted_file_name = decrypted_file_name;
+} 
---
+++ src/pdvout/pdvout.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+
+#include "decrypt.cpp"
+#include "inflate.cpp"
+#include "pdvout.cpp"
+#include "information.cpp"
+
+void
+	startPdv(std::string&),
+	decryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::string&),
+	inflateFile(std::vector<uint_fast8_t>&),
+	displayInfo();
