59c5a376e28003ed6286ce510e7ca7f3d6db4a62
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-30 15:40:07 +0100

    Add files via upload

---
+++ src/pdvout/pdvout.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <algorithm>
+#include <filesystem>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+
+constexpr uint_fast8_t XOR_KEY_LENGTH = 12;
+
+#include "four_bytes.cpp"
+#include "decrypt.cpp"
+#include "inflate.cpp"
+#include "pdvout.cpp"
+#include "information.cpp"
+
+std::string decryptFile(std::vector<uint_fast8_t>&, uint_fast8_t (&)[XOR_KEY_LENGTH], std::string&);
+
+uint_fast32_t getFourByteValue(const std::vector<uint_fast8_t>&, uint_fast32_t);
+
+void
+	startPdv(const std::string&),
+	inflateFile(std::vector<uint_fast8_t>&),
+	displayInfo();
