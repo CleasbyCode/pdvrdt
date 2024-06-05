d5a566a46fe9ad2df5fc5083110db2f6c2f9055c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-05 15:14:38 +0100

    Update decryptFile.cpp

--- src/pdvout/decryptFile.cpp
+++ src/pdvout/decryptFile.cpp
@@ -15,7 +15,6 @@ std::string decryptFile(std::vector<uint_fast8_t>&Image_Vec, uint_fast8_t (&xor_
 	std::string decrypted_data_filename;
 
 	while (ENCRYPTED_DATA_FILE_SIZE > index_pos) {
-		
 		if (index_pos >= ENCRYPTED_DATA_FILENAME_LENGTH) {
 			name_key_pos = name_key_pos >= ENCRYPTED_DATA_FILENAME_LENGTH ? 0 : name_key_pos;
 		} else {
