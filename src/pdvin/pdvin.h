506bdf37ca7d4acd8636fcbd73e0ed7dd9c84cac
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-05 15:03:41 +0100

    Add files via upload

---
+++ src/pdvin/pdvin.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include <filesystem>
+#include <random>
+#include <cstdint>
+#include <algorithm>
+#include <iostream>
+#include <string_view>
+#include <string>
+#include <regex>
+#include <fstream>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+
+#include "fourBytes.cpp"
+#include "crc32.cpp"
+#include "searchFunc.cpp"
+#include "eraseChunks.cpp"
+#include "encryptFile.cpp"
+#include "deflateFile.cpp"
+#include "valueUpdater.cpp"
+#include "pdvin.cpp"
+#include "information.cpp"
+
+template <uint_fast8_t N>
+uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+
+uint_fast32_t
+	crcUpdate(uint_fast8_t*, uint_fast32_t),
+	encryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::string&),
+	deflateFile(std::vector<uint_fast8_t>&),
+	getFourByteValue(const std::vector<uint_fast8_t>&, const uint_fast32_t);
+	
+void 
+	startPdv(const std::string&, std::string&, bool, bool),
+	eraseChunks(std::vector<uint_fast8_t>&),
+	valueUpdater(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t),
+	displayInfo();
+
