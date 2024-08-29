f3cb50d165fe3fd1d5e262eb3d0cf21d4f45f82c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-30 00:54:42 +0100

    Update main.cpp

--- src/pdvin/main.cpp
+++ src/pdvin/main.cpp
@@ -17,20 +17,22 @@ int main(int argc, char** argv) {
 			isMastodonOption = (argc > 3 && std::string(argv[1]) == "-m") ? true : false,
 			isRedditOption = (argc > 3 && !isMastodonOption) ? true : false;
 
-		const std::regex REG_EXP("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+		constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+		const std::regex regex_pattern(REG_EXP);
 
-		const std::string
-			IMAGE_FILENAME = isMastodonOption || isRedditOption ? argv[2] : argv[1],
-			IMAGE_FILE_EXTENSION = IMAGE_FILENAME.length() > 2 ? IMAGE_FILENAME.substr(IMAGE_FILENAME.length() - 3) : IMAGE_FILENAME;
+		const std::string IMAGE_FILENAME = isMastodonOption || isRedditOption ? argv[2] : argv[1];
 
 		std::string data_filename = isMastodonOption || isRedditOption ? argv[3] : argv[2];
 
-		if (IMAGE_FILE_EXTENSION != "png" || (argc > 3 && std::string(argv[1]) != "-r" && std::string(argv[1]) != "-m")) {
-			std::cerr << (IMAGE_FILE_EXTENSION != "png" 
+		std::filesystem::path image_path(IMAGE_FILENAME);
+		std::string image_extension = image_path.extension().string();
+
+		if (image_extension != ".png" || (argc > 3 && std::string(argv[1]) != "-r" && std::string(argv[1]) != "-m")) {
+			std::cerr << (image_extension != ".png" 
 				? "\nFile Type Error: Invalid file extension. Expecting only \"png\""
 				: "\nInput Error: Invalid arguments. Expecting only -m or -r") 
 			<< ".\n\n";
-		} else if (!regex_match(IMAGE_FILENAME, REG_EXP) || !regex_match(data_filename, REG_EXP)) {
+		} else if (!regex_match(IMAGE_FILENAME, regex_pattern) || !regex_match(data_filename, regex_pattern)) {
 			std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
 		} else if (!std::filesystem::exists(IMAGE_FILENAME) || !std::filesystem::exists(data_filename) || !std::filesystem::is_regular_file(data_filename)) {
 			std::cerr << (!std::filesystem::exists(IMAGE_FILENAME) 
