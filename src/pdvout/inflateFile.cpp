0d8a8f3f015e5fe2571938a91e8805be8fa0fe68
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 19:59:48 +0100

    Update inflateFile.cpp

--- src/pdvout/inflateFile.cpp
+++ src/pdvout/inflateFile.cpp
@@ -1,7 +1,6 @@
 // zlib function, see https://zlib.net/
 void inflateFile(std::vector<uint_fast8_t>& Vec) {
-
-	constexpr uint32_t BUFSIZE = 2097152;
+	const uint_fast32_t BUFSIZE = static_cast<uint_fast32_t>(Vec.size());
 	uint_fast8_t* buffer{ new uint_fast8_t[BUFSIZE] };
 
 	std::vector<uint_fast8_t>Inflate_Vec;
@@ -9,7 +8,7 @@ void inflateFile(std::vector<uint_fast8_t>& Vec) {
 	z_stream strm;
 	strm.zalloc = 0;
 	strm.zfree = 0;
-	strm.avail_in = static_cast<uint_fast32_t>(Vec.size());
+	strm.avail_in = BUFSIZE;
 	strm.next_in = Vec.data();
 	strm.next_out = buffer;
 	strm.avail_out = BUFSIZE;
@@ -18,13 +17,12 @@ void inflateFile(std::vector<uint_fast8_t>& Vec) {
 
 	while (strm.avail_in) {
 		inflate(&strm, Z_NO_FLUSH);
-
 		if (!strm.avail_out) {
 			Inflate_Vec.insert(Inflate_Vec.end(), buffer, buffer + BUFSIZE);
 			strm.next_out = buffer;
 			strm.avail_out = BUFSIZE;
 		} else {
-		    break;
+			break;
 		}
 	}
 
@@ -33,6 +31,5 @@ void inflateFile(std::vector<uint_fast8_t>& Vec) {
 	inflateEnd(&strm);
 
 	delete[] buffer;
-	Vec.swap(Inflate_Vec);
-	
+	Vec.swap(Inflate_Vec);	
 }
