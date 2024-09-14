7e83f7b69506c2c676084e0852f7c1454e7418db
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 19:41:59 +0100

    Update deflateFile.cpp

--- src/pdvin/deflateFile.cpp
+++ src/pdvin/deflateFile.cpp
@@ -1,7 +1,7 @@
 // zlib function, see https://zlib.net/
 uint_fast32_t deflateFile(std::vector<uint_fast8_t>& Vec) {
 
-	constexpr uint_fast32_t BUFSIZE = 2097152;
+	const uint_fast32_t BUFSIZE = static_cast<uint_fast32_t>(Vec.size());
 	uint_fast8_t* buffer{ new uint_fast8_t[BUFSIZE] };
 
 	std::vector<uint_fast8_t>Deflate_Vec;
@@ -9,13 +9,13 @@ uint_fast32_t deflateFile(std::vector<uint_fast8_t>& Vec) {
 	z_stream strm;
 	strm.zalloc = 0;
 	strm.zfree = 0;
-	strm.avail_in = static_cast<uint_fast32_t>(Vec.size());
+	strm.avail_in = BUFSIZE;
 	strm.next_in = Vec.data();
 	strm.next_out = buffer;
 	strm.avail_out = BUFSIZE;
 
-	deflateInit(&strm, Z_DEFAULT_COMPRESSION); 
-	
+	deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+
 	while (strm.avail_in) {
 		deflate(&strm, Z_NO_FLUSH);		
 		if (!strm.avail_out) {
