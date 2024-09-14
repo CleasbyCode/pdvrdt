b74b6919522cacb2c4a100f32f97701bc4e1e69c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 17:04:28 +0100

    Update main.cpp

--- src/pdvout/main.cpp
+++ src/pdvout/main.cpp
@@ -9,29 +9,37 @@
 #include "pdvout.h"
 
 int main(int argc, char** argv) {
-	if (argc !=2) {
+	if (argc != 2) {
 		std::cout << "\nUsage: pdvout <file_embedded_image>\n\t\bpdvout --info\n\n";
-	} else if (std::string(argv[1]) == "--info") {
-		displayInfo();
-	} else {
-		constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
-		const std::regex regex_pattern(REG_EXP);
-
-		const std::string IMAGE_FILENAME = std::string(argv[1]);
-
-		std::filesystem::path image_path(IMAGE_FILENAME);
-		std::string image_extension = image_path.extension().string();
-
-		if (image_extension == ".png" && regex_match(IMAGE_FILENAME, regex_pattern) && std::filesystem::exists(IMAGE_FILENAME)) {
-			pdvOut(IMAGE_FILENAME);
-		} else {
-			std::cerr << (image_extension != ".png" 
-				? "\nFile Type Error: Invalid file extension. Expecting only \"png\""
-		   		: !regex_match(IMAGE_FILENAME, regex_pattern) 
-					? "\nInvalid Input Error: Characters not supported by this program found within filename arguments"
-						: "\nImage File Error: File not found. Check the filename and try again")
-			<< ".\n\n";
-			return 1;
-		}
+		return 1;
 	}
+
+	if (std::string(argv[1]) == "--info") {
+		displayInfo();
+		return 0;
+	}
+
+	const std::string IMAGE_FILENAME = std::string(argv[1]);
+
+	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+	const std::regex regex_pattern(REG_EXP);
+
+	if (!std::regex_match(IMAGE_FILENAME, regex_pattern)) {
+		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
+        	return 1;
+	}
+
+	std::filesystem::path image_path(IMAGE_FILENAME);
+	const std::string IMAGE_EXTENSION = image_path.extension().string();
+
+	if (IMAGE_EXTENSION != ".png") {
+		std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"png\" image extension.\n\n";
+		return 1;
+	}
+	
+	if (!std::filesystem::exists(IMAGE_FILENAME)) {
+		std::cerr << "\nImage File Error: File not found. Check the filename and try again.\n\n";
+		return 1;
+	}
+	pdvOut(IMAGE_FILENAME);
 }
