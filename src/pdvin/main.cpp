0ca3a17cfa5fe4b0f08b11c915a1030dc3d7aba8
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 16:42:47 +0100

    Update main.cpp

--- src/pdvin/main.cpp
+++ src/pdvin/main.cpp
@@ -18,27 +18,18 @@ int main(int argc, char** argv) {
 		std::cout << "\nUsage: pdvin [-m|-r] <cover_image> <data_file>\n\t\bpdvin --info\n\n";
 		return 1;
 	}
-	
 	const bool
 		isMastodonOption = (argc > 3 && std::string(argv[1]) == "-m"),
 		isRedditOption = (argc > 3 && std::string(argv[1]) == "-r"),
 		isInvalidOption = (argc > 3 && !(isMastodonOption || isRedditOption));
 
 	if (isInvalidOption) {
-		std::cerr << "\nInput Error: Invalid arguments. Expecting only \"-m\" or \"-r\" as the first optional argument.\n\n";
+		std::cerr << "\nInput Error: Invalid arguments. Expecting only \"-m\" or \"-r\" as the first argument option.\n\n";
 		return 1;
 	}
-	
+
 	const std::string IMAGE_FILENAME = isMastodonOption || isRedditOption ? argv[2] : argv[1];
 	std::string data_filename = isMastodonOption || isRedditOption ? argv[3] : argv[2];
-
-	std::filesystem::path image_path(IMAGE_FILENAME);
-	std::string image_extension = image_path.extension().string();
-	
-	if (image_extension != ".png") {
-		std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"png\" image extension.\n\n";
-		return 1;
-	}
 
 	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
 	const std::regex regex_pattern(REG_EXP);
@@ -47,13 +38,21 @@ int main(int argc, char** argv) {
 		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
 		return 1;
 	}
-	
+
+	std::filesystem::path image_path(IMAGE_FILENAME);
+	std::string image_extension = image_path.extension().string();
+
+	if (image_extension != ".png") {
+		std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"png\" image extension.\n\n";
+		return 1;
+	}
+
 	if (!std::filesystem::exists(IMAGE_FILENAME) || !std::filesystem::exists(data_filename) || !std::filesystem::is_regular_file(data_filename)) {
 		std::cerr << (!std::filesystem::exists(IMAGE_FILENAME)
 			? "\nImage File Error: File not found."
-            		: "\nData File Error: File not found or not a regular file.")
-            	<< " Check the filename and try again.\n\n";
+			: "\nData File Error: File not found or not a regular file.")
+			<< " Check the filename and try again.\n\n";
 		return 1;
-    }
+	}
 	pdvIn(IMAGE_FILENAME, data_filename, isMastodonOption, isRedditOption);			
 }
