ac831ffaf1177c752104867df4f7f431e02e51a1
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 00:24:16 +0100

    Add files via upload

---
+++ src/pdvout/decryptFile.cpp
@@ -0,0 +1,27 @@
+std::string decryptFile(std::vector<uint_fast8_t>&Image_Vec, uint_fast8_t (&xor_key)[XOR_KEY_LENGTH], uint_fast32_t file_size, uint_fast8_t filename_length, std::string& filename) {
+
+	// const uint_fast32_t ENCRYPTED_DATA_FILE_SIZE = static_cast<uint_fast32_t>(Image_Vec.size());
+
+	// const uint_fast8_t ENCRYPTED_DATA_FILENAME_LENGTH = static_cast<uint_fast8_t>(encrypted_data_filename.length());
+
+	uint_fast8_t
+		xor_key_pos = 0,
+		name_pos = 0;
+
+	std::string decrypted_filename;
+
+	while (filename_length--) {
+		decrypted_filename += filename[name_pos++] ^ xor_key[xor_key_pos++];
+	}
+
+	uint_fast32_t
+		decrypt_pos = 0,
+		index_pos = 0;
+
+	while (file_size--) {
+		Image_Vec[decrypt_pos++] = Image_Vec[index_pos++] ^ xor_key[xor_key_pos++];
+		xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;	
+	}
+
+	return decrypted_filename;
+} 
---
+++ src/pdvout/pdvout.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <algorithm>
+#include <filesystem>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+
+constexpr uint_fast8_t XOR_KEY_LENGTH = 24;
+
+#include "getByteValue.cpp"
+#include "searchFunc.cpp"
+#include "decryptFile.cpp"
+#include "inflateFile.cpp"
+#include "pdvout.cpp"
+#include "information.cpp"
+
+std::string decryptFile(std::vector<uint_fast8_t>&, uint_fast8_t (&)[XOR_KEY_LENGTH], uint_fast32_t, uint_fast8_t, std::string&);
+
+template <uint_fast8_t N>
+uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+
+uint_fast32_t getByteValue(const std::vector<uint_fast8_t>&, const uint_fast32_t);
+
+void
+	inflateFile(std::vector<uint_fast8_t>&),
+	displayInfo();
+
+uint_fast8_t pdvOut(const std::string&);
