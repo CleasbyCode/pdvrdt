afa2e83dd1f177d22163ca25187a0321a6f32e03
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 23:56:15 +0100

    Update pdvout.h

--- src/pdvout/pdvout.h
+++ src/pdvout/pdvout.h
@@ -20,15 +20,15 @@ constexpr uint_fast8_t XOR_KEY_LENGTH = 24;
 #include "pdvout.cpp"
 #include "information.cpp"
 
-std::string decryptFile(std::vector<uint_fast8_t>&, uint_fast8_t (&)[XOR_KEY_LENGTH], uint_fast32_t, uint_fast8_t, std::string&);
+std::string decryptFile(std::vector<uint8_t>&, uint_fast8_t (&)[XOR_KEY_LENGTH], uint_fast32_t, uint_fast8_t, std::string&);
 
 template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+uint_fast32_t searchFunc(std::vector<uint8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
 
-uint_fast32_t getByteValue(const std::vector<uint_fast8_t>&, const uint_fast32_t);
+uint_fast32_t getByteValue(const std::vector<uint8_t>&, const uint_fast32_t);
 
 void
-	inflateFile(std::vector<uint_fast8_t>&),
+	inflateFile(std::vector<uint8_t>&),
 	displayInfo();
 
 uint_fast8_t pdvOut(const std::string&);
