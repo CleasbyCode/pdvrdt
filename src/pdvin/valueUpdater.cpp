6368e18669018c1c51a1b5166d278fd2661c0d5e
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-21 18:04:41 +0100

    Update valueUpdater.cpp

--- src/pdvin/valueUpdater.cpp
+++ src/pdvin/valueUpdater.cpp
@@ -1,6 +1,6 @@
 // Writes updated values, such as chunk lengths, CRC, etc. into the relevant vector index locations.
 void valueUpdater(std::vector<uint8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t NEW_VALUE, uint_fast8_t bits) {
-	while (value_bit_length) {
+	while (bits) {
 		vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff;
 	}
 }
