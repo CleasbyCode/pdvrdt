c5764b87be85e7efe46d5fe505316f760e8bee9f
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-04 17:06:25 +0100

    Update encryptFile.cpp

--- src/pdvin/encryptFile.cpp
+++ src/pdvin/encryptFile.cpp
@@ -1,21 +1,20 @@
-uint_fast32_t encryptFile(std::vector<uint8_t>&Profile_Data_Vec, std::vector<uint8_t>&File_Vec, uint_fast32_t data_file_size, std::string& data_filename) {
-
+uint32_t encryptFile(std::vector<uint8_t>&Profile_Data_Vec, std::vector<uint8_t>&File_Vec, uint32_t data_file_size, std::string& data_filename) {
 	std::random_device rd;
  	std::mt19937 gen(rd());
 	std::uniform_int_distribution<unsigned short> dis(1, 255); 
 	
-	constexpr uint_fast8_t XOR_KEY_LENGTH = 24;
+	constexpr uint8_t XOR_KEY_LENGTH = 24;
 		
-	uint_fast8_t xor_key[XOR_KEY_LENGTH];
+	uint8_t xor_key[XOR_KEY_LENGTH];
 
-	uint_fast16_t xor_key_profile_insert_index = 0x197;
+	uint16_t xor_key_profile_insert_index = 0x197;
 		
-	for (uint_fast8_t i = 0; XOR_KEY_LENGTH > i; ++i) {
-        	xor_key[i] = static_cast<uint_fast8_t>(dis(gen));
+	for (uint8_t i = 0; XOR_KEY_LENGTH > i; ++i) {
+        	xor_key[i] = static_cast<uint8_t>(dis(gen));
 		Profile_Data_Vec[xor_key_profile_insert_index++] = xor_key[i];
     	}
 
-	uint_fast8_t
+	uint8_t
 		profile_name_index = 0x65,
 		data_filename_length = Profile_Data_Vec[profile_name_index - 1],
 		xor_key_pos = 0,
@@ -26,7 +25,7 @@ uint_fast32_t encryptFile(std::vector<uint8_t>&Profile_Data_Vec, std::vector<uin
 		Profile_Data_Vec[profile_name_index++] = data_filename[name_pos++];
 	}
 
-	uint_fast32_t index_pos = 0;
+	uint32_t index_pos = 0;
 	
 	Profile_Data_Vec.reserve(Profile_Data_Vec.size() + data_file_size);
 
@@ -34,5 +33,5 @@ uint_fast32_t encryptFile(std::vector<uint8_t>&Profile_Data_Vec, std::vector<uin
 		Profile_Data_Vec.emplace_back(File_Vec[index_pos++] ^ xor_key[xor_key_pos++]);
 		xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;
 	}
-	return static_cast<uint_fast32_t>(Profile_Data_Vec.size());
+	return static_cast<uint32_t>(Profile_Data_Vec.size());
 }	
