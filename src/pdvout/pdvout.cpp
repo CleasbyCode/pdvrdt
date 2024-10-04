b14cea4c8ef91f42e14e384bc7f91fb86553c350
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-04 17:29:52 +0100

    Update pdvout.cpp

--- src/pdvout/pdvout.cpp
+++ src/pdvout/pdvout.cpp
@@ -1,10 +1,10 @@
-uint_fast8_t pdvOut(const std::string& IMAGE_FILENAME) {
+uint8_t pdvOut(const std::string& IMAGE_FILENAME) {
 
-	constexpr uint_fast32_t 
+	constexpr uint32_t 
 		MAX_FILE_SIZE 	= 2684354560, // 2.5GB.
 		LARGE_FILE_SIZE = 419430400;  // 400MB.
 
-	constexpr uint_fast8_t MIN_FILE_SIZE = 68;
+	constexpr uint8_t MIN_FILE_SIZE = 68;
 
 	const size_t IMAGE_FILE_SIZE = std::filesystem::file_size(IMAGE_FILENAME);
 
@@ -32,32 +32,31 @@ uint_fast8_t pdvOut(const std::string& IMAGE_FILENAME) {
 
 	std::copy(std::istreambuf_iterator<char>(image_ifs), std::istreambuf_iterator<char>(), std::back_inserter(Image_Vec));
 
-	constexpr uint_fast8_t
+	constexpr uint8_t
 		PNG_SIG[] 	{ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A },
 		PNG_IEND_SIG[]	{ 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82 },
 		ICCP_SIG[] 	{ 0x69, 0x43, 0x43, 0x50, 0x69, 0x63, 0x63 },
 		IDAT_FILE_SIG[] { 0x49, 0x44, 0x41, 0x54, 0x5E, 0x78 };
 		
-	if (!std::equal(std::begin(PNG_SIG), std::end(PNG_SIG), std::begin(Image_Vec)) 
-		|| !std::equal(std::begin(PNG_IEND_SIG), std::end(PNG_IEND_SIG), std::end(Image_Vec) - 8)) {
-        		std::cerr << "\nImage File Error: Signature check failure. This file is not a valid PNG image.\n\n";
+	if (!std::equal(std::begin(PNG_SIG), std::end(PNG_SIG), std::begin(Image_Vec)) || !std::equal(std::begin(PNG_IEND_SIG), std::end(PNG_IEND_SIG), std::end(Image_Vec) - 8)) {
+        	std::cerr << "\nImage File Error: Signature check failure. This file is not a valid PNG image.\n\n";
 		return 1;
     	}
 
-	constexpr uint_fast8_t ICCP_CHUNK_INDEX = 0x25;
+	constexpr uint8_t ICCP_CHUNK_INDEX = 0x25;
 
-	const uint_fast32_t
+	const uint32_t
 		ICCP_POS = searchFunc(Image_Vec, 0, 0, ICCP_SIG),
 		IDAT_POS = searchFunc(Image_Vec, 0, 0, IDAT_FILE_SIG);
 
-	if (ICCP_POS != ICCP_CHUNK_INDEX && IDAT_POS == static_cast<uint_fast32_t>(Image_Vec.size())) {
+	if (ICCP_POS != ICCP_CHUNK_INDEX && IDAT_POS == static_cast<uint32_t>(Image_Vec.size())) {
 		std::cerr << "\nImage File Error: This is not a pdvrdt file-embedded image.\n\n";
 		return 1;
 	}
 
 	bool isMastodonFile = (ICCP_POS == ICCP_CHUNK_INDEX);
 	
-	const uint_fast32_t
+	const uint32_t
 		CHUNK_SIZE_INDEX = isMastodonFile ? ICCP_POS - 4 : IDAT_POS - 4,				
 		DEFLATE_DATA_INDEX = isMastodonFile ? ICCP_POS + 9 : IDAT_POS + 5,		
 		CHUNK_SIZE = getByteValue(Image_Vec, CHUNK_SIZE_INDEX),
@@ -73,11 +72,11 @@ uint_fast8_t pdvOut(const std::string& IMAGE_FILENAME) {
 		return 1;
 	}
 
-	constexpr uint_fast16_t
+	constexpr uint16_t
 		PDV_SIG_INDEX = 0x191,		
 		DATA_FILE_INDEX = 0x1AF;
 
-	constexpr uint_fast8_t 
+	constexpr uint8_t 
 		ENCRYPTED_FILENAME_INDEX = 0x65,
 		PDV_SIG[] { 0x50, 0x44, 0x56, 0x52, 0x44, 0x54 };
 
@@ -86,14 +85,14 @@ uint_fast8_t pdvOut(const std::string& IMAGE_FILENAME) {
 		return 1;
 	}
 
-	uint_fast16_t xor_key_index = 0x197;
+	uint16_t xor_key_index = 0x197;
 
-	uint_fast8_t 
+	uint8_t 
 		xor_key[XOR_KEY_LENGTH],
 		encrypted_filename_length = Image_Vec[ENCRYPTED_FILENAME_INDEX - 1];
 	
 	// Get the xor_key from the profile.
-	for (uint_fast8_t i = 0; i < XOR_KEY_LENGTH;) {
+	for (uint8_t i = 0; i < XOR_KEY_LENGTH;) {
 		xor_key[i++] = Image_Vec[xor_key_index++]; 	
     	}
 
@@ -101,7 +100,7 @@ uint_fast8_t pdvOut(const std::string& IMAGE_FILENAME) {
 
 	Image_Vec.erase(Image_Vec.begin(), Image_Vec.begin() + DATA_FILE_INDEX);
 
-	uint_fast32_t file_size = static_cast<uint_fast32_t>(Image_Vec.size());
+	uint32_t file_size = static_cast<uint32_t>(Image_Vec.size());
 
 	const std::string DECRYPTED_DATA_FILENAME = decryptFile(Image_Vec, xor_key, file_size, encrypted_filename_length, encrypted_filename);	
 	
