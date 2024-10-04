b6cdbd78ea695f88a9ba00c5e4ae503b7ded0def
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-04 17:10:56 +0100

    Update main.cpp

--- src/pdvin/main.cpp
+++ src/pdvin/main.cpp
@@ -1,10 +1,11 @@
-//	PNG Data Vehicle (pdvin v1.1). Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023
+//	PNG Data Vehicle (pdvin v1.2). Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023
 // 
 //	Compile program (Linux)
 //	$ g++ main.cpp -O2 -lz -s -o pdvin
-// 
+//      $ sudo cp pdvin /usr/bin
+
 //	Run it
-//	$ ./pdvin
+//	$ pdvin
 
 #include "pdvin.h"
 
