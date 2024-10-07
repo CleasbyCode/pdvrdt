18b4406adc3ca940f1ec88158f72c48dd6f534a2
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-07 10:20:09 +0100

    Update information.cpp

--- src/pdvin/information.cpp
+++ src/pdvin/information.cpp
@@ -12,11 +12,11 @@ $ g++ main.cpp -O2 -lz -s -o pdvin
 $ sudo cp pdvin /usr/bin
 $ pdvin
 
-Usage: pdvin [-m] [-r] <cover_image> <data_file>  
+Usage: pdvin [-m|-r] <cover_image> <data_file>  
        pdvin --info
 	
-Post your file-embedded image on the following compatible sites.  
-Image size limits(cover image + data file):
+Post your "file-embedded" PNG image on the following compatible sites.  
+Image size limits (cover image + data file):
 
 Flickr (200MB), ImgBB (32MB), PostImage (32MB), Reddit (19MB / -r option), 
 Mastodon (16MB / -m option), ImgPile (8MB), X/Twitter (5MB / Dimension limits).
@@ -24,21 +24,18 @@ Mastodon (16MB / -m option), ImgPile (8MB), X/Twitter (5MB / Dimension limits).
 Argument options:	
 		
 To create "file-embedded" PNG images compatible for posting on:
-Reddit. Use the -r option with pdvin.
-Mastodon. Use the -m option with pdvin.
-		
- -m = Mastodon option, (pdvin -m cover_image.png my_hidden_file.mp3).
- -r = Reddit option,   (pdvin -r cover_image.png my_hidden_file.doc).
+Mastodon. Use the -m option (pdvin -m cover_image.png my_hidden_file.mp3).
+Reddit. Use the -r option (pdvin -r cover_image.png my_hidden_file.doc).
 		
 From the Reddit site, click "Create Post" then select "Images & Video" tab, to post your PNG image.
 		
-X/Twitter - As well as the 5MB PNG image size limit, X/Twitter also has dimension size limits.
+In addition to the 5MB image size limit, X/Twitter also has the following dimension size limits.
 
-PNG-32/24 (Truecolor) 900x900 Max. 68x68 Min. 
-PNG-8 (Indexed color) 4096x4096 Max. 68x68 Min.
+PNG-32/24 (Truecolor) 68x68 Min. - 900x900 Max.
+PNG-8 (Indexed color) 68x68 Min. - 4096x4096 Max.
 
-ImgPile - You must sign in to an account before sharing your file-embedded PNG image on this platform.
-Sharing your image without logging in, your embedded file will not be preserved.
+ImgPile - You must sign in to an account before sharing your "file-embedded" PNG image on this site.
+Sharing your image without logging in, your embedded data will not be preserved.
 
 )";
 }
