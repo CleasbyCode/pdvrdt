da87012d64fd7fd9da9068402bef851bed418f76
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-02-07 21:27:29 +0000

    Update information.cpp

--- src/pdvout/information.cpp
+++ src/pdvout/information.cpp
@@ -1,14 +1,15 @@
 void displayInfo() {
 	std::cout << R"(
 
-PNG Data Vehicle (pdvout v1.3). 
+PNG Data Vehicle (pdvout v3.2). 
 Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
 
 A steganography-like CLI tool to extract hidden data from a (pdvin) PNG image. 
 
 Compile & run pdvout (Linux):
 		
-$ g++ main.cpp -O2 -lz -s -o pdvout
+$ sudo apt-get install libsodium-dev
+$ g++ main.cpp -O2 -lz -lsodium -s -o pdvout
 $ sudo cp pdvout /usr/bin
 $ pdvout
 
