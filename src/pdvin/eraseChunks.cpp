1cb6e40d1f8569a5198b9ed25eb25350b7acef78
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-02-08 08:45:46 +0000

    Update eraseChunks.cpp

--- src/pdvin/eraseChunks.cpp
+++ src/pdvin/eraseChunks.cpp
@@ -1,17 +1,16 @@
-// Erase superfluous PNG chunks from the cover image. Just keep the critical PNG chunks/data. (HEADER/IHDR/*PLTE/IDAT/IEND).
 void eraseChunks(std::vector<uint8_t>& Vec, const size_t VEC_SIZE) {
 	constexpr uint8_t 
 		PLTE_SIG[] { 0x50, 0x4C, 0x54, 0x45 },
 		IDAT_SIG[] { 0x49, 0x44, 0x41, 0x54 },
 		PNG_FIRST_BYTES 	= 33,
 		PNG_IEND_CHUNK_LENGTH 	= 12,
-		COMBINED_FIELD_LENGTHS 	= 12, // Length (4), Name (4), CRC (4).
+		COMBINED_FIELD_LENGTHS 	= 12, 
 		INC_NEXT_SEARCH_INDEX 	= 5;
 	
 	std::vector<uint8_t>Temp_Vec;
 	Temp_Vec.reserve(VEC_SIZE);
 
-	Temp_Vec.insert(Temp_Vec.begin(), Vec.begin(), Vec.begin() + PNG_FIRST_BYTES); // Copy first 33 bytes of cover image into Temp_Vec. PNG header and IHDR chunk.
+	Temp_Vec.insert(Temp_Vec.begin(), Vec.begin(), Vec.begin() + PNG_FIRST_BYTES); 
 
 	auto copyChunk = [&](const uint8_t (&SIG)[4]) {
 		uint32_t chunk_name_index = PNG_FIRST_BYTES;
