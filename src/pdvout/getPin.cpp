372e1793ddc5143f6159b4600436475fd242ab3e
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-02-08 08:54:23 +0000

    Update getPin.cpp

--- src/pdvout/getPin.cpp
+++ src/pdvout/getPin.cpp
@@ -1,5 +1,4 @@
 uint64_t getPin() {
-
     constexpr uint64_t MAX_UINT64 = std::numeric_limits<uint64_t>::max();
     std::string input;
     char ch; 
@@ -7,57 +6,50 @@ uint64_t getPin() {
 
 #ifdef _WIN32
     while (input.length() < 20) { 
-	 ch = _getch();
-        if (ch >= '0' && ch <= '9') {
-            input.push_back(ch);
-            std::cout << '*' << std::flush;  
-        } else if (ch == '\b' && !input.empty()) {  
-            std::cout << "\b \b" << std::flush;  
-            input.pop_back();
-        } else if (ch == '\r') {
-            break;
-        }
-
+	    ch = _getch();
+	    if (ch >= '0' && ch <= '9') {
+		    input.push_back(ch);
+		    std::cout << '*' << std::flush;
+	    } else if (ch == '\b' && !input.empty()) {
+		    std::cout << "\b \b" << std::flush;
+		    input.pop_back();
+	    } else if (ch == '\r') {
+		    break;
+	    }
     }
 #else
-   
-    struct termios oldt, newt;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-
-    newt.c_lflag &= ~(ICANON | ECHO);
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
+	struct termios oldt, newt;
+    	tcgetattr(STDIN_FILENO, &oldt);
+    	newt = oldt;
+    	newt.c_lflag &= ~(ICANON | ECHO);
+    	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
    while (input.length() < 20) {
-        ssize_t bytesRead = read(STDIN_FILENO, &ch, 1); 
-        if (bytesRead <= 0) continue; 
-       
-        if (ch >= '0' && ch <= '9') {
-            input.push_back(ch);
-            std::cout << '*' << std::flush; 
-        } else if ((ch == '\b' || ch == 127) && !input.empty()) {  
-            std::cout << "\b \b" << std::flush;
-            input.pop_back();
-        } else if (ch == '\n') {
-            break;
-        }
-    }
+	   ssize_t bytesRead = read(STDIN_FILENO, &ch, 1); 
+	   if (bytesRead <= 0) continue; 
+	   
+	   if (ch >= '0' && ch <= '9') {
+		   input.push_back(ch);
+		   std::cout << '*' << std::flush; 
+	   } else if ((ch == '\b' || ch == 127) && !input.empty()) {  
+		   std::cout << "\b \b" << std::flush;
+		   input.pop_back();
+	   } else if (ch == '\n') {
+		   break;
+	   }
+   }
 
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
 
 #endif
 
-    std::cout << std::endl; 
+std::cout << std::endl; 
+std::cout.sync_with_stdio(sync_status);
 
-    std::cout.sync_with_stdio(sync_status);
-
-    uint64_t user_pin;
-    if (input.empty() || input.length() > 20 || std::stoull(input) > MAX_UINT64) {
-        user_pin = 0; 
-    } else {
-        user_pin = std::stoull(input); 
-    }
-    return user_pin;
+uint64_t user_pin;
+if (input.empty() || input.length() > 20 || std::stoull(input) > MAX_UINT64) {
+	user_pin = 0; 
+} else {
+	user_pin = std::stoull(input); 
+}
+	return user_pin;
 }
