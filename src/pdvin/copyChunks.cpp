4fa345d39b326f66acaefe4649aa9aa27a8b9ddf
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-04-16 14:03:21 +0100

    Add files via upload

--- src/pdvin/copyChunks.cpp
+++ src/pdvin/copyChunks.cpp
@@ -1,4 +1,4 @@
-void copyEssentialPngChunks(std::vector<uint8_t>& image_vec, const size_t IMAGE_VEC_SIZE) {
+void copyEssentialChunks(std::vector<uint8_t>& image_vec, const size_t IMAGE_VEC_SIZE) {
 	constexpr std::array<uint8_t, 4> 
 		PLTE_SIG {0x50, 0x4C, 0x54, 0x45},
 		IDAT_SIG {0x49, 0x44, 0x41, 0x54};
@@ -27,23 +27,18 @@ void copyEssentialPngChunks(std::vector<uint8_t>& image_vec, const size_t IMAGE_
 
         	while (true) {
             		chunk_search_pos = searchFunc(image_vec, chunk_search_pos, SEARCH_INCREMENT, chunk_signature);
-            		
 			if (chunk_search_pos == IMAGE_VEC_SIZE) {
 				break;
 			}
-
 			chunk_length_pos = chunk_search_pos - PNG_CHUNK_LENGTH_FIELD_SIZE;
             		chunk_length = getByteValue<uint32_t>(image_vec, chunk_length_pos) + PNG_CHUNK_FIELDS_OVERHEAD;
 	    		std::copy_n(image_vec.begin() + chunk_length_pos, chunk_length, std::back_inserter(copied_image_vec));
         	}
     	};
-
 	if (image_vec[PNG_COLOR_TYPE_INDEX] == PNG_INDEXED_COLOR_VAL) {
     		copy_chunk_type(PLTE_SIG);
 	}
-
     	copy_chunk_type(IDAT_SIG);
-
     	copied_image_vec.insert(copied_image_vec.end(), image_vec.end() - PNG_IEND_BYTES, image_vec.end());
     	image_vec = std::move(copied_image_vec);
 }
