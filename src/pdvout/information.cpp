255c59060fa1148afb39b05eeb95113bdacff958
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-06-16 21:32:53 +0100

    Update information.cpp

--- src/pdvout/information.cpp
+++ src/pdvout/information.cpp
@@ -4,7 +4,7 @@
 void displayInfo() {
 	std::cout << R"(
 
-PNG Data Vehicle (pdvout v3.4). 
+PNG Data Vehicle (pdvout v3.5). 
 Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
 
 A steganography-like CLI tool to extract hidden data from a (pdvin) PNG image. 
