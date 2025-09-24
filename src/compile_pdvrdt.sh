fd5d36b5e81e5523a9dc83dc1a7dba16c97d65f9
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-09-24 12:54:27 +0100

    Update compile_pdvrdt.sh

--- src/compile_pdvrdt.sh
+++ src/compile_pdvrdt.sh
@@ -2,7 +2,7 @@
 
 # compile_pdvin.sh
 
-g++ -std=c++20 pdvrdt.cpp lodepng/lodepng.cpp -Wall -O2 -lz -lsodium -s -o pdvrdt
+g++ -std=c++20 pdvrdt.cpp lodepng/lodepng.cpp -Wall -O3 -lz -lsodium -s -o pdvrdt
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable 'pdvrdt' created."
